b14a33f0f8d6e3849959b461eb726586015951b3
Vivek Pernamitta <quic_vpernami@quicinc.com>
2023-03-13 19:53:12 +0530

    mhi: bus: msm: Increase event ring-10 size to 4096
    Increase event ring-10 size to 4096 from 2048 elements.
    Event ring-10 corresponds to hardware chan:106, when DP driver
    submits data at high speed traffic and polls for event ring
    to process channel completion, at certain point of time
    Device GSI hardware is stuck as there are no event ring elements
    for GSI hardware to update.
    
    Change-Id: I5cfdd3c1630f93dc03d647478afda0f78997d7f9
    Signed-off-by: Vivek Pernamitta <quic_vpernami@quicinc.com>

--- drivers/bus/mhi/pci_generic.c
+++ drivers/bus/mhi/pci_generic.c
@@ -220,7 +220,7 @@ static struct mhi_event_config modem_qcom_v1_mhi_lsn_vf_events[] = {
 			 true, false, false, 104),
 	MHI_EVENT_CONFIG(9, 8, MHI_ER_DATA, 2048, 0, MHI_DB_BRST_DISABLE,
 			 true, true, false, 105),
-	MHI_EVENT_CONFIG(10, 9, MHI_ER_DATA, 2048, 0, MHI_DB_BRST_DISABLE,
+	MHI_EVENT_CONFIG(10, 9, MHI_ER_DATA, 4096, 0, MHI_DB_BRST_DISABLE,
 			 true, false, false, 106),
 	MHI_EVENT_CONFIG(11, 10, MHI_ER_DATA, 2048, 0, MHI_DB_BRST_DISABLE,
 			 true, false, false, 107),
