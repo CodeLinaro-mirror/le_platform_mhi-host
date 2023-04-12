870ff8f27cb86d30d465985b4ef660669449c91f
Qiang Yu <quic_qianyu@quicinc.com>
2023-04-12 11:51:18 +0800

    Create firmware directory according to the argument
    From the installer and cross-compiler scripts which output the
    installer script, create the firmware directory according to
    the first argument.
    
    Change-Id: I14065c630b483c236683e6121db1c83c4915fc85
    Signed-off-by: Qiang Yu <quic_qianyu@quicinc.com>

--- build/cross_compile.sh
+++ build/cross_compile.sh
@@ -85,7 +85,7 @@ if [[ ! -f "out/install.sh" ]]; then
 	echo "echo Installing following kernel modules for \$$uname_str:" >> out/install.sh
 	echo "echo \$(cat \$$uname_str/etc/modules | tr '\n' ' ')" >> out/install.sh
 	echo "cat \$$uname_str/etc/modules | xargs -I % sh -c 'grep -qxF % /etc/modules || echo % >> /etc/modules'" >> out/install.sh
-	echo "mkdir -p /lib/firmware/qcom/sdx65m" >> out/install.sh
+	echo "if [[ \$1 == SDX65 ]] ; then mkdir -p /lib/firmware/qcom/sdx65m; elif [[ \$1 == SDX75 ]]; then mkdir -p /lib/firmware/qcom/sdx75m; fi" >> out/install.sh
 	echo "cp \$$uname_str/etc/udev/rules.d/*.rules /etc/udev/rules.d/" >> out/install.sh
 	echo "udevadm control --reload" >> out/install.sh
 	echo "cp \$$uname_str/etc/mhi_dynamicdebug.sh /etc/mhi_dynamicdebug.sh" >> out/install.sh
